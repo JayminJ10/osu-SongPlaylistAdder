d273644b69b42a3cf23da7627fc17bad0675448e
Leopold67 <jayminjhaveri10@gmail.com>
2024-03-19 01:22:08 -0400

    Song Gen

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
-import googleapiclient as gapi
+import YT_Search as YT
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def generate_songs():
+    path = (os.getenv('TEMPPATH'))
+    file = open('songs.txt', 'w+')
+    for folder in os.listdir(path):
+        file.write(folder + "\n")
+
+if __name__ == "__main__":
+    if not os.path.exists('./songs.txt'):
+        generate_songs()
